9809e5378f3db80688fec1c926c08f35e3a7c040
Rohan Goel <rohangoel@Rohans-MacBook-Pro.local>
2025-05-26 23:28:28 +0530

    Added functionality to handle new line character in the string

--- src/calculatorAdd.ts
+++ src/calculatorAdd.ts
@@ -1,15 +1,23 @@
 export class Calculator {
     add(numbers: string): number {
-      if (!numbers) {
-        return 0;
-      }
+        if (!numbers) {
+            return 0;
+        }
 
-    // Split by comma and convert to numbers
-    const numberArray = numbers
-      .split(',')
-      .map(num => parseInt(num.trim(), 10))
+        let delimiters = [',', '\n'];
+        let numbersToProcess = numbers;
 
-    // Sum all numbers
-    return numberArray.reduce((sum, num) => sum + num, 0);
+        // Replace each delimiter with a comma.
+        delimiters.forEach(delimiter => {
+            numbersToProcess = numbersToProcess.split(delimiter).join(',');
+        });
+
+        // Split by comma and convert to numbers
+        const numberArray = numbers
+            .split(',')
+            .map(num => parseInt(num.trim(), 10))
+
+        // Sum all numbers
+        return numberArray.reduce((sum, num) => sum + num, 0);
     }
 }
--- test/calculatorAdd.test.ts
+++ test/calculatorAdd.test.ts
@@ -22,4 +22,12 @@ describe("Calculator", () => {
       it('should handle any amount of numbers', () => {
         expect(calculator.add('1,2,3,4,5')).toBe(15);
       });
+
+      it('should handle new line character between numbers', () => {
+        expect(calculator.add('1\n2,3')).toBe(6);
+      });
+
+      it('should handle nmultiple new line characters between numbers', () => {
+        expect(calculator.add('1\n2\n3')).toBe(6);
+      });
 });
